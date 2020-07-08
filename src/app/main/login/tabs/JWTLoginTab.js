55a8e8c5ad0067c4599e398916243242e59d3704
Chaitnya <chaitnyaparikh@gmail.com>
2020-07-09 01:00:58 +0530

    feat(fixes): removed typology

--- src/app/main/login/tabs/JWTLoginTab.js
+++ src/app/main/login/tabs/JWTLoginTab.js
@@ -136,58 +136,6 @@ function JWTLoginTab(props) {
 					Login{'  '} {login.loadingLogin && <CircularProgress size={15} color="secondary" />}
 				</Button>
 			</Formsy>
-
-			<div className="flex flex-col items-center pt-24">
-				<Typography className="text-14 font-600 py-8">Credentials</Typography>
-
-				<Divider className="mb-16 w-256" />
-
-				<table className="text-left w-256">
-					<thead>
-						<tr>
-							<th>
-								<Typography className="font-600" color="textSecondary">
-									Role
-								</Typography>
-							</th>
-							<th>
-								<Typography className="font-600" color="textSecondary">
-									Username
-								</Typography>
-							</th>
-							<th>
-								<Typography className="font-600" color="textSecondary">
-									Password
-								</Typography>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>
-								<Typography>Admin</Typography>
-							</td>
-							<td>
-								<Typography>admin</Typography>
-							</td>
-							<td>
-								<Typography>admin</Typography>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<Typography>Staff</Typography>
-							</td>
-							<td>
-								<Typography>staff</Typography>
-							</td>
-							<td>
-								<Typography>staff</Typography>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
 		</div>
 	);
 }
