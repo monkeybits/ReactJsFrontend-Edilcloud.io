09db7df76e6f75e53d68e77c87e81663e2ed17a1
Chaitnya <chaitnyaparikh@gmail.com>
2020-07-24 12:45:33 +0530

    fix(UI_fix): contact fix solve filter

--- src/app/main/apps/contacts/ContactsSidebarContent.js
+++ src/app/main/apps/contacts/ContactsSidebarContent.js
@@ -10,7 +10,9 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import * as Actions from './store/actions';
+import { useSelector, useDispatch } from 'react-redux';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => ({
 	listItem: {
@@ -36,11 +38,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function ContactsSidebarContent(props) {
+	const dispatch = useDispatch();
 	const user = useSelector(({ contactsApp }) => contactsApp.user);
 	const company = useSelector(({ chatApp }) => chatApp.company);
+	const filterKey = useSelector(({ contactsApp }) => contactsApp.contacts.filterKey);
 
 	const classes = useStyles(props);
-
+	const getListItemClassName = key => (key == filterKey ? clsx(classes.listItem, 'active') : classes.listItem);
 	return (
 		<div className="p-0 lg:p-24 lg:ltr:pr-4 lg:rtl:pl-4">
 			<FuseAnimate animation="transition.slideLeftIn" delay={200}>
@@ -56,81 +60,61 @@ function ContactsSidebarContent(props) {
 					<List>
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/all"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('all'))}
+							className={getListItemClassName('all')}
 						>
 							<ListItemText className="truncate" primary="All Team members" disableTypography />
 						</ListItem>
-						<Divider />
+
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/approved"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('approved'))}
+							className={getListItemClassName('approved')}
 						>
 							<ListItemText className="truncate" primary="Approved Team members" disableTypography />
 						</ListItem>
-						<Divider />
+
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/waiting"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('waiting'))}
+							className={getListItemClassName('waiting')}
 						>
 							<ListItemText className="truncate" primary="Waiting Team members" disableTypography />
 						</ListItem>
-						<Divider />
+
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/refused"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('refused'))}
+							className={getListItemClassName('refused')}
 						>
 							<ListItemText className="truncate" primary="Refused Team members" disableTypography />
 						</ListItem>
 						<Divider />
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/owner"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('owner'))}
+							className={getListItemClassName('owner')}
 						>
 							<ListItemText className="truncate" primary="Owner" disableTypography />
 						</ListItem>
-						<Divider />
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/delegate"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('delegate'))}
+							className={getListItemClassName('delegate')}
 						>
 							<ListItemText className="truncate" primary="Delegate" disableTypography />
 						</ListItem>
-						<Divider />
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/manager"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('manager'))}
+							className={getListItemClassName('manager')}
 						>
 							<ListItemText className="truncate" primary="Manager" disableTypography />
 						</ListItem>
-
-						<Divider />
 						<ListItem
 							button
-							component={NavLinkAdapter}
-							to="/apps/contacts/worker"
-							activeClassName="active"
-							className={classes.listItem}
+							onClick={() => dispatch(Actions.filterByKey('worker'))}
+							className={getListItemClassName('worker')}
 						>
 							<ListItemText className="truncate" primary="Worker" disableTypography />
 						</ListItem>
