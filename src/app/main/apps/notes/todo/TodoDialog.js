83f7acf0dea81e00071d8a7b598f1984d0ec9a1b
Chaitnya <chaitnyaparikh@gmail.com>
2020-08-10 15:41:37 +0530

    fix(project_app): added percentage slider

--- src/app/main/apps/notes/todo/TodoDialog.js
+++ src/app/main/apps/notes/todo/TodoDialog.js
@@ -28,6 +28,7 @@ import * as Actions from './store/actions';
 import FuseChipSelect from '@fuse/core/FuseChipSelect';
 import { useParams } from 'react-router';
 import DatePicker from 'react-datepicker';
+import { Slider, withStyles } from '@material-ui/core';
 
 const defaultFormState = {
 	id: '',
@@ -41,7 +42,93 @@ const defaultFormState = {
 	deleted: false,
 	labels: []
 };
+const iOSBoxShadow = '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
 
+const marks = [
+	{
+		value: 0
+	},
+	{
+		value: 10
+	},
+	{
+		value: 20
+	},
+	{
+		value: 30
+	},
+	{
+		value: 40
+	},
+	{
+		value: 50
+	},
+	{
+		value: 60
+	},
+	{
+		value: 70
+	},
+	{
+		value: 80
+	},
+	{
+		value: 90
+	},
+	{
+		value: 100
+	}
+];
+
+const IOSSlider = withStyles({
+	root: {
+		color: '#3880ff',
+		height: 2,
+		padding: '15px 0'
+	},
+	thumb: {
+		height: 28,
+		width: 28,
+		backgroundColor: '#fff',
+		boxShadow: iOSBoxShadow,
+		marginTop: -14,
+		marginLeft: -14,
+		'&:focus, &:hover, &$active': {
+			boxShadow: '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+			// Reset on touch devices, it doesn't add specificity
+			'@media (hover: none)': {
+				boxShadow: iOSBoxShadow
+			}
+		}
+	},
+	active: {},
+	valueLabel: {
+		left: 'calc(-50% + 12px)',
+		top: -22,
+		'& *': {
+			background: 'transparent',
+			color: '#000'
+		}
+	},
+	track: {
+		height: 2
+	},
+	rail: {
+		height: 2,
+		opacity: 0.5,
+		backgroundColor: '#bfbfbf'
+	},
+	mark: {
+		backgroundColor: '#bfbfbf',
+		height: 8,
+		width: 1,
+		marginTop: -3
+	},
+	markActive: {
+		opacity: 1,
+		backgroundColor: 'currentColor'
+	}
+})(Slider);
 function TodoDialog(props) {
 	const dispatch = useDispatch();
 	const todoDialog = useSelector(({ todoApp }) => todoApp.todos.todoDialog);
@@ -139,7 +226,7 @@ function TodoDialog(props) {
 	}
 
 	function canBeSubmitted() {
-		return form.title.length > 0 && taskDate.startDate 
+		return form.title.length > 0 && taskDate.startDate;
 	}
 
 	return (
@@ -330,6 +417,7 @@ function TodoDialog(props) {
 							}}
 						/>
 					</div>
+					<IOSSlider aria-label="ios slider" defaultValue={0} marks={marks} valueLabelDisplay="on" />
 				</div>
 			</DialogContent>
 
