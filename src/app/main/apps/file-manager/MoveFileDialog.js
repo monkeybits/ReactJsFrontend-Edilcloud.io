be9a2526dba63ea148da3e9e78fd140b276a69a2
Chaitnya <chaitnyaparikh@gmail.com>
2020-09-04 21:04:10 +0530

    fix(project_app): removed unused declaration

--- src/app/main/apps/file-manager/MoveFileDialog.js
+++ src/app/main/apps/file-manager/MoveFileDialog.js
@@ -1,81 +1,22 @@
-import FuseAnimate from '@fuse/core/FuseAnimate';
-import FusePageSimple from '@fuse/core/FusePageSimple';
-import Fab from '@material-ui/core/Fab';
-import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
-import withReducer from 'app/store/withReducer';
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Breadcrumb from './Breadcrumb';
-import DetailSidebarContent from './DetailSidebarContent';
-import DetailSidebarHeader from './DetailSidebarHeader';
-import FileList from './FileList';
-import MainSidebarContent from './MainSidebarContent';
-import MainSidebarHeader from './MainSidebarHeader';
 import * as Actions from './store/actions';
-import reducer from './store/reducers';
-import { makeStyles, Button, TextField, CircularProgress, LinearProgress } from '@material-ui/core';
-import {
-	ADD_PHOTO,
-	ADD_FOLDER,
-	ADD_VIDEO,
-	ADD_DOCUMENT,
-	MOVE_PHOTO_FILE,
-	MOVE_VIDEO_FILE,
-	MOVE_DOCUMENT_FILE
-} from 'app/services/apiEndPoints';
+import { Button, TextField } from '@material-ui/core';
+import { MOVE_PHOTO_FILE, MOVE_VIDEO_FILE, MOVE_DOCUMENT_FILE } from 'app/services/apiEndPoints';
 import { METHOD, apiCall } from 'app/services/baseUrl';
-import { getHeaderToken, decodeDataFromToken, getCompressFile } from 'app/services/serviceUtils';
-import { withRouter } from 'react-router';
+import { getHeaderToken } from 'app/services/serviceUtils';
 
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import Paper from '@material-ui/core/Paper';
-import LinearProgressWithLabel from './LinearProgressWithLabel';
-import TransitionAlerts from './TransitionAlerts.js';
-import FloatingButtonUpload from './FloatingButtonUpload';
-import imageCompression from 'browser-image-compression';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
-import * as ContactActions from './store/actions';
-const styles = theme => ({
-	root: {
-		margin: 0,
-		padding: theme.spacing(2)
-	},
-	closeButton: {
-		position: 'absolute',
-		right: theme.spacing(1),
-		top: theme.spacing(1),
-		color: theme.palette.grey[500]
-	}
-});
-
-const DialogTitle = withStyles(styles)(props => {
-	const { children, classes, onClose, ...other } = props;
-	return (
-		<MuiDialogTitle disableTypography className={classes.root} {...other}>
-			<Typography variant="h6">{children}</Typography>
-			{onClose ? (
-				<IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-					<CloseIcon />
-				</IconButton>
-			) : null}
-		</MuiDialogTitle>
-	);
-});
 
 const DialogContent = withStyles(theme => ({
 	root: {
@@ -94,13 +35,13 @@ const DialogActions = withStyles(theme => ({
 		}
 	}
 }))(MuiDialogActions);
-function MoveFileDialog(props) {
+function MoveFileDialog() {
 	const dispatch = useDispatch();
 	const moveFileDialog = useSelector(({ fileManagerApp }) => fileManagerApp.files.moveFileDialog);
 	const files = useSelector(({ fileManagerApp }) => fileManagerApp.files);
 	const folderPath = useSelector(({ fileManagerApp }) => fileManagerApp.files.folderPath);
 	const currentFolderPath = files.folders?.filter(folder => folder.path == folderPath[folderPath.length - 1]);
-	const [error, seterror] = useState({
+	const [error] = useState({
 		fileError: '',
 		titleError: '',
 		descError: '',
@@ -108,14 +49,6 @@ function MoveFileDialog(props) {
 		apiError: ''
 	});
 	const [path, setPath] = useState('');
-	const [filePath, setFilePath] = useState('');
-	const [title, setTitle] = useState(undefined);
-	const [description, setDescription] = useState(undefined);
-	const initDialog = useCallback(() => {
-		console.log({
-			moveFileDialog
-		});
-	}, [moveFileDialog.data, moveFileDialog.type]);
 
 	useEffect(() => {
 		/**
@@ -127,9 +60,7 @@ function MoveFileDialog(props) {
 		dispatch(Actions.closeMoveFileDialog());
 	}
 
-	function canBeSubmitted() {}
-
-	const handleMoveFile = event => {
+	const handleMoveFile = () => {
 		let fileType = moveFileDialog.data.type;
 		let fileId = moveFileDialog.data.id;
 		let relative_path = moveFileDialog.data.relative_path;
@@ -152,18 +83,6 @@ function MoveFileDialog(props) {
 		);
 	};
 
-	function handleRemove() {}
-	const resetError = () =>
-		seterror({
-			fileError: '',
-			titleError: '',
-			descError: '',
-			nameError: ''
-		});
-	const [state, setState] = React.useState({
-		checkedA: true,
-		checkedB: true
-	});
 	const { relative_path } = moveFileDialog.data;
 	return (
 		<Dialog
@@ -204,7 +123,7 @@ function MoveFileDialog(props) {
 							style={{ width: '100%' }}
 							className="mb-24"
 							getOptionLabel={option => option.path}
-							renderOption={(option, { selected }) => <>{option.path}</>}
+							renderOption={option => <>{option.path}</>}
 							renderInput={params => <TextField {...params} label="Path" />}
 							onInputChange={(e, value) => setPath(value)}
 							variant="outlined"
