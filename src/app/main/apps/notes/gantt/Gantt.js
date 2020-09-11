6c11b108507ba4917d82140343079422dd462e46
Chaitnya <chaitnyaparikh@gmail.com>
2020-09-11 18:28:30 +0530

    feat(project_app): gantt tasks list

--- src/app/main/apps/notes/gantt/Gantt.js
+++ src/app/main/apps/notes/gantt/Gantt.js
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import { gantt } from 'dhtmlx-gantt';
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
 import './Gantt.css';
+import connect from 'react-redux/es/connect/connect';
+import moment from 'moment';
 
-export default class Gantt extends Component {
+// data: [
+// 	{ id: 1, text: 'Task #1', start_date: '15-04-2019', duration: 3, progress: 0.6 },
+// 	{ id: 2, text: 'Task #2', start_date: '18-04-2019', duration: 3, progress: 0.4 }
+// ],
+// links: [{ id: 1, source: 1, target: 2, type: '0' }]
+class Gantt extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			tasks: undefined
+		};
+	}
+
 	componentDidMount() {
-		gantt.config.xml_date = '%Y-%m-%d %H:%i';
-		const { tasks } = this.props;
-		gantt.init(this.ganttContainer);
-		gantt.parse(tasks);
+		// gantt.config.xml_date = '%Y-%m-%d %H:%i';
+		// const { tasks } = this.props;
+		// gantt.init(this.ganttContainer);
+		// gantt.parse(tasks);
 	}
 	shouldComponentUpdate(nextProps) {
+		const { todos } = nextProps;
+		if (!this.state.tasks) {
+			let tasks = {
+				data: Object.values(todos.entities).map((data, index) => ({
+					...{
+						id: index + 1,
+						text: data.name,
+						start_date: data.date_start,
+						end_date: data.date_end,
+						duration: moment(data.date_start).diff(moment(data.date_end)),
+						progress: data.progress / 100,
+						mainId: data.id
+					}
+				}))
+			};
+			gantt.config.xml_date = '%Y-%m-%d %H:%i';
+			gantt.init(this.ganttContainer);
+			gantt.parse(tasks);
+			this.setState({
+				tasks
+			});
+		}
 		return this.props.zoom !== nextProps.zoom;
 	}
 
@@ -54,9 +91,16 @@ export default class Gantt extends Component {
 				ref={input => {
 					this.ganttContainer = input;
 				}}
-                style={{ width: '100%', height: '100%' }}
-                className="gantt-min-height"
+				style={{ width: '100%', height: '100%' }}
+				className="gantt-min-height"
 			></div>
 		);
 	}
 }
+function mapStateToProps({ todoAppNote }) {
+	return {
+		todos: todoAppNote.todos
+	};
+}
+
+export default connect(mapStateToProps)(Gantt);
--- src/app/main/apps/notes/gantt/index.js
+++ src/app/main/apps/notes/gantt/index.js
@@ -1,2 +1,23 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDeepCompareEffect } from '@fuse/hooks';
 import Gantt from './Gantt';
-export default Gantt;
+import * as Actions from '../todo/store/actions';
+import { useParams } from 'react-router';
+
+function GanttWrapper(props) {
+    const dispatch = useDispatch();
+	const routeParams = useParams();
+    
+	useDeepCompareEffect(() => {
+		dispatch(Actions.getTodos(routeParams.id));
+		return () => {
+			dispatch({
+				type: Actions.GET_TODOS,
+				payload: []
+			});
+		};
+	}, [dispatch, routeParams]);
+	return <Gantt {...props} />;
+}
+export default GanttWrapper;
