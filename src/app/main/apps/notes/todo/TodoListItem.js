bd0abbb5134fdd92219aba0223d4e6d2200cdef9
monkeybits <67373899+monkeybits@users.noreply.github.com>
2020-09-16 22:55:36 +0200

    Update TodoListItem.js
    readd footer left

--- src/app/main/apps/notes/todo/TodoListItem.js
+++ src/app/main/apps/notes/todo/TodoListItem.js
@@ -239,7 +239,12 @@ function TodoListItem(props) {
 				<div className="flex justify-between h-48 px-16 border-t-1">
 					{/* left side footer */}
 					
-
+					<div className="flex items-center -mx-6">
+						<div class="flex items-center px-8 py-4 mx-4 rounded-sm bg-grey-700 text-white">
+							<Icon className="text-16">check_circle</Icon>
+							<span class="mx-4">2/7</span>
+						</div>
+					</div>
 					{/* right side footer */}
 					<div className="flex items-center justify-end -mx-6">
 						{props.todo.assigned_company?.id == company.id && (
