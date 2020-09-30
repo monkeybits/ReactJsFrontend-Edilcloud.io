af4e1242f8e5444f21bf85c10bca4a63d571149d
tbellini01 <Oneplus3t>
2020-09-30 11:10:36 +0200

    # added OneSignal sdk add external user id

--- src/app/main/apps/scrumboard/boards/Boards.js
+++ src/app/main/apps/scrumboard/boards/Boards.js
@@ -115,6 +115,10 @@ function Boards(props) {
 		);
 	};
 	const redirectAfterGetNewToken = company_profile_id => {
+		const myCustomUniqueUserId = company_profile_id;
+		OneSignal.push(function() {
+			OneSignal.setExternalUserId(myCustomUniqueUserId);
+		});
 		apiCall(
 			REFRESH_TOKEN(company_profile_id),
 			{
