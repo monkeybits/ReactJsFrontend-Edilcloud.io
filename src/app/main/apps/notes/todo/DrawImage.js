4a64bcddb15bd01e97606b0c76c0db9937d87eb4
Gulam Nasir Shaikh <gulam@peerbits.com>
2020-11-18 19:14:57 +0530

    Bug solved on Drawer image page

--- src/app/main/apps/notes/todo/DrawImage.js
+++ src/app/main/apps/notes/todo/DrawImage.js
@@ -14,15 +14,9 @@ import DialogActions from '@material-ui/core/DialogActions';
 import { addLine } from './line';
 import { addTextNode } from './textNode';
 import Image from './Image';
-import { Button, ButtonGroup } from '@material-ui/core';
+import { Button, ButtonGroup, Modal } from '@material-ui/core';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Icon from '@material-ui/core/Icon';
-
-import Modal from 'react-bootstrap/Modal';
-import ModalHeader from 'react-bootstrap/ModalHeader'
-import ModalTitle from 'react-bootstrap/ModalTitle'
-import ModalBody from 'react-bootstrap/ModalBody'
-import ModalFooter from 'react-bootstrap/ModalFooter'
 
 const uuidv1 = require('uuid/v1');
 
@@ -40,6 +34,7 @@ function DrawImage({ open, onClose, imgSrc, replaceUrl, width, height }) {
 	let layerEl = React.createRef();
 	const fileUploadEl = React.createRef();
 	let imageRef = React.useRef();
+	
 	const [imageProps, setImageProps] = useState({
 		height: 0,
 		width: 0
@@ -260,12 +255,23 @@ function DrawImage({ open, onClose, imgSrc, replaceUrl, width, height }) {
 
 	return (
 		<>
-			<Modal size="lg" show={open} onHide={onClose} animation={false} className="rs-dialog-sm-full zoom-125 custom-drawing-modal">
-				<Modal.Header closeButton>
-					<Modal.Title>Drawing</Modal.Title>
-				</Modal.Header>
-				<Modal.Body id="dialog-content" scrollable="true">
-				<ButtonGroup className="mb-12">
+			<Dialog open={open} onClose={onClose} fullWidth className="rs-dialog-sm-full zoom-125 custom-drawing-modal">
+				<DialogTitle component="div" className="p-0">
+					<AppBar position="static" elevation={1}>
+						<Toolbar className="flex w-full overflow-x-auto px-8 sm:px-16">
+							<div className="flex flex-1">
+								<div className="my-12">
+									<div className="font-size-18">Drawing</div>
+								</div>
+							</div>
+							<IconButton onClick={onClose} color="inherit">
+								<Icon>close</Icon>
+							</IconButton>
+						</Toolbar>
+					</AppBar>
+				</DialogTitle>
+				<DialogContent id="dialog-content" className="p-0 sm:p-10">
+					<ButtonGroup className="mb-12">
 						<Button variant="primary" onClick={drawLine}>
 							Line
 						</Button>
@@ -283,15 +289,28 @@ function DrawImage({ open, onClose, imgSrc, replaceUrl, width, height }) {
 						</Button>
 					</ButtonGroup>
 					<input style={{ display: 'none' }} type="file" ref={fileUploadEl} onChange={fileChange} />
+					{/* <Stage
+			  width={imageProps.width}
+			  height={imageProps.height}
+			  ref={stageEl}
+			  onMouseDown={(e) => {
+				// deselect when clicked on empty area
+				const clickedOnEmpty = e.target === e.target.getStage();
+				if (clickedOnEmpty) {
+				  selectShape(null);
+				}
+			  }}
+			> */}
+
 					<Stage
-						// style={{ overFlow: 'scroll', background: '#DFD9D9'}}
+						style={{ overFlow: 'scroll', background: '#DFD9D9' }}
 						width={imageProps.width}
-						height={imageProps.height}
+						height={imageProps.height}						
 						onWheel={handleWheel}
 						scaleX={stageScale}
 						scaleY={stageScale}
-						x={stageX}
-						y={stageY}
+						// x={stageX}
+						// y={stageY}
 						ref={ref => {
 							stageEl = ref;
 						}}
@@ -325,9 +344,9 @@ function DrawImage({ open, onClose, imgSrc, replaceUrl, width, height }) {
 							})}
 						</Layer>
 					</Stage>
-				</Modal.Body>
-				<Modal.Footer>
-				<Button
+				</DialogContent>
+				<DialogActions className="p-16 px-20">
+					<Button
 					variant="contained"
 					color="primary"
 						onClick={() => {
@@ -344,8 +363,8 @@ function DrawImage({ open, onClose, imgSrc, replaceUrl, width, height }) {
 					>
 						save
 					</Button>
-				</Modal.Footer>
-			</Modal>
+				</DialogActions>
+			</Dialog>
 		</>
 	);
 }
