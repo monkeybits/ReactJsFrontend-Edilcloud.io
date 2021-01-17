c7651d8a99f4ad618d3815f55ec79d8aa711e5e5
tommasobellini <dev@tommasobellini.it>
2021-01-17 23:52:17 +0100

    fix fb login button

--- src/app/main/pages/auth/login/FacebookLoginComponent.js
+++ src/app/main/pages/auth/login/FacebookLoginComponent.js
@@ -76,7 +76,6 @@ class FacebookLoginComponent extends React.Component {
 					appId="1093743794410189"
 					// autoLoad={true}
 					fields="name,email,picture"
-					scope="public_profile,user_friends,user_actions.books"
 					callback={this.responseFacebook}
 					onFailure={err => console.log(err)}
 					icon={<img src="/assets/images/social-icons/facebook.png" className="h-20" alt="Facebook" />}
