c0681a1d52ff19c2a5e0d6fdcb1b418d710e8aed
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-03-11 12:25:37 +0530

    Added webpack package and set configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,33 @@
-const cssLoaders = [parts.tailwind()];
-const commonConfig = merge([
-  parts.extractCSS({ loaders: cssLoaders }),
-]);
-
-const productionConfig = merge([parts.eliminateUnusedCSS()]);
+module.exports = {
+  //...
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: Infinity,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          }
+        },
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+};
