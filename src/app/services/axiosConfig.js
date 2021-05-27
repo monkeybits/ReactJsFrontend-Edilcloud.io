dfa6c0bebbda46a0023fd0a04a3498a94eb2392c
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-05-27 15:22:54 +0530

    Added lock file.

--- src/app/services/axiosConfig.js
+++ src/app/services/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://back-test.edilcloud.io'
-	// process.env.NODE_ENV !== 'production' ? process.env.REACT_APP_BASE_URL_LOCAL : process.env.REACT_APP_BASE_URL
+	// baseURL: 'https://back-test.edilcloud.io'
+	baseURL: process.env.NODE_ENV === 'production'
+		? process.env.REACT_APP_BASE_URL_PROD
+		: process.env.REACT_APP_BASE_URL
 });
 export default instance;
