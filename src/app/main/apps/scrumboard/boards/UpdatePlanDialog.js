ac85452516d8fe92f3fd4cfb24322ee46cbacc0e
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-06-08 17:47:03 +0530

    Make modal responsive and resolved purchase plan issue.

--- src/app/main/apps/scrumboard/boards/UpdatePlanDialog.js
+++ src/app/main/apps/scrumboard/boards/UpdatePlanDialog.js
@@ -88,7 +88,8 @@ function UpdatePlanDialog(props) {
 		) {
 			props.history.push('/apps/billing');
 		} else {
-			window.location = `${process.env.REACT_APP_BASE_URL}/api/frontend/payments/customer-portal?customer_id=${upgradePlanDetail?.id}`;
+			console.log('upgradePlanDetail', upgradePlanDetail)
+			window.location = `${process.env.REACT_APP_BASE_URL}/api/frontend/payments/customer-portal?customer_id=${upgradePlanDetail?.customer}`;
 			// props.history.push(`https://back.edilcloud.io/api/frontend/payments/customer-portal?customer_id=${company?.customer}`);
 		}
 	};
@@ -106,34 +107,34 @@ function UpdatePlanDialog(props) {
 				<IconButton aria-label="close" className="absolute right-0 top-0" onClick={handleClose}>
 					<CloseIcon />
 				</IconButton>
-				<Typography className="text-4xl font-bold text-center mt-12">Upgrade to Standard</Typography>
+				<Typography className="text-xl sm:text-4xl font-bold text-center mt-12">Upgrade to Standard</Typography>
 				<div>
-					<Typography className="text-lg text-center">
+					<Typography className="text-md sm:text-lg text-center">
 						Empower your team with these additional features.
 					</Typography>
-					<div className="flex items-center">
+					<div className="sm:flex items-center">
 						<div>
-							<img className="w-400" src="assets/images/images.jpg" />
+							<img className="w-400 mx-auto" src="assets/images/images.jpg" />
 						</div>
 						<div className="ml-16">
-							<ListItem className="grid ">
-								<Typography className="text-18 p-4 flex items-center">
+							<ListItem className="grid justify-center">
+								<Typography className="text-15 xs:text-18 sm:text-15 md:text-18 p-4 flex items-center">
 									<Icon className="mr-8">fiber_manual_record_rounded</Icon> Plan your work in one
 									visual timeline.
 								</Typography>
-								<Typography className="text-18 p-4 flex items-center">
+								<Typography className="text-15 xs:text-18 sm:text-15 md:text-18 p-4 flex items-center">
 									<Icon className="mr-8">fiber_manual_record_rounded</Icon>Share boards with people
 									outside your team.
 								</Typography>
-								<Typography className="text-18 p-4 flex items-center">
+								<Typography className="text-15 xs:text-18 sm:text-15 md:text-18 p-4 flex items-center">
 									<Icon className="mr-8">fiber_manual_record_rounded</Icon>Filter your boards however
 									you choose.
 								</Typography>
-								<Typography className="text-18 p-4 flex items-center">
+								<Typography className="text-15 xs:text-18 sm:text-15 md:text-18 p-4 flex items-center">
 									<Icon className="mr-8">fiber_manual_record_rounded</Icon>Send emails directly to add
 									updates.
 								</Typography>
-								<Typography className="text-18 p-4 flex items-center">
+								<Typography className="text-15 xs:text-18 sm:text-15 md:text-18 p-4 flex items-center">
 									<Icon className="mr-8">fiber_manual_record_rounded</Icon>Connect dapulse with your
 									other apps
 								</Typography>
@@ -142,11 +143,11 @@ function UpdatePlanDialog(props) {
 					</div>
 					<div className="flex mt-24 justify-between">
 						<div className="grid justify-start">
-							<Typography className="text-lg">
+							<Typography className="sm:text-lg">
 								For only <b>$20</b> more a month
 							</Typography>
-							<Typography className="text-base">you can supercharge your team's productivity.</Typography>
-							<Typography className="text-8 font-bold">
+							<Typography className="sm:text-base">you can supercharge your team's productivity.</Typography>
+							<Typography className="sm:text-8 font-bold">
 								Want to add more team mates? <span className="text-blue-500">see pricing</span>
 							</Typography>
 						</div>
@@ -154,7 +155,7 @@ function UpdatePlanDialog(props) {
 							<Button
 								onClick={onConfirmPayment}
 								variant="contained"
-								className="justify-center d-inline-block mb-10 bg-blue-500 text-white normal-case rounded-32 w-200"
+								className="justify-center d-inline-block mb-10 bg-blue-500 text-white normal-case rounded-32 sm:w-200 text-12 sm:text-14 p-0"
 							>
 								Confirm upgrade
 								{/* {loading && <CircularProgress size={20} color="secondary" />} */}
