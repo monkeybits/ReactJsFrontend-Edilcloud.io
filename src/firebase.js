2735c840f1faf5554949b9b25df642347284a2e5
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-06-09 13:53:49 +0530

    update firebase key pair key.

--- src/firebase.js
+++ src/firebase.js
@@ -24,7 +24,8 @@ if (!firebase.apps.length) {
 const messaging = firebase.messaging();
 
 export const getToken = (setTokenFound) => {
-    return messaging.getToken({vapidKey: 'BHRyW3ZRnv9giFMD9DFotLg-TNT0HsH4txDPjhnkQrRc6n_n_PlakUB4hMZdqVWDvsWM3GYhsxlDowYPd8LBCPo'}).then((currentToken) => {
+    // return messaging.getToken({vapidKey: 'BHRyW3ZRnv9giFMD9DFotLg-TNT0HsH4txDPjhnkQrRc6n_n_PlakUB4hMZdqVWDvsWM3GYhsxlDowYPd8LBCPo'}).then((currentToken) => {
+    return messaging.getToken({vapidKey: 'BHRyW3ZRnv9giFMD9DFotLg-BMMbkB-0wGOQ_ycUIRyI0GIKg22-QuoDC6RLVZjOIXGSnKbQMNaOZr4dL3ZLSwtACLvDL8Rsg9aoHT-pu6syQ_0'}).then((currentToken) => {
       if (currentToken) {
         console.log('current token for client: ', currentToken);
         setTokenFound(true);
