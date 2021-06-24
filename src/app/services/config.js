d07b6d326b171d5ad03fc8b80fd57fcd7da54788
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-06-24 18:00:45 +0530

    Added websocket url.

---
+++ src/app/services/config.js
@@ -0,0 +1,5 @@
+export const WS_BASE = process.env.REACT_APP_WEBSOCKET_URL + '/ws/chat/chat_channel/';
+
+export const WS_BASE_NOTIFICATION = process.env.REACT_APP_WEBSOCKET_URL + '/ws/notify/notify_channel/';
+
+export const WS_BASE_PROJECT_REPORT = process.env.REACT_APP_WEBSOCKET_URL + '/ws/report/report_channel/';
