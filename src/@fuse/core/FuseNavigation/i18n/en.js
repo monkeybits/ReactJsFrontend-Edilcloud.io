3d932e965da644b70bbc2e9d219ff8ba4cf19e6c
Tejal Limbani <tejal.limbani@tatvasoft.com>
2021-07-15 15:45:10 +0530

    Ios message changes.

---
+++ src/@fuse/core/FuseNavigation/i18n/en.js
@@ -0,0 +1,6 @@
+const locale = {
+	UPDATE_MESSAGE: 'You can update your account manager from browser.',
+    COPY_TEXT: 'Click on the link to copy it '
+};
+
+export default locale;
---
+++ src/@fuse/core/FuseNavigation/i18n/it.js
@@ -0,0 +1,6 @@
+const locale = {
+	UPDATE_MESSAGE: 'Puoi aggiornare il tuo account manager da browser.',
+    COPY_TEXT: 'Clicca sul link per copiarlo '
+};
+
+export default locale;
